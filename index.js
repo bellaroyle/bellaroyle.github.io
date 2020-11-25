3f85760c17215465dbc00fef5dbb67261a0f9af2
bella <bella.royle@gmail.com>
2020-11-25 17:11:33 +0000

    added nav and footer

---
+++ index.js
@@ -0,0 +1,12 @@
+
+// func to make nav bar dissapear when scroll down but appear when scroll up 
+let prevScrollPosition = window.pageYOffset;
+window.onscroll = function() {
+  let currentScrollPosition = window.pageYOffset;
+  if (prevScrollPosition > currentScrollPosition) {
+    document.getElementById("navbar").style.top = "0";
+  } else {
+    document.getElementById("navbar").style.top = "-50px";
+  }
+  prevScrollPosition = currentScrollPosition;
+}
